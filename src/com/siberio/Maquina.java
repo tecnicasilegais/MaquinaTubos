c081dbb7c73289f6feecc31a1e4f7c858ba5dfa5
ITWhiteRaccoon <eduardo.a@edu.pucrs.br>
2019-11-11 12:18:23 -0300

    Update Maquina.java

--- src/com/siberio/Maquina.java
+++ src/com/siberio/Maquina.java
@@ -1,5 +1,15 @@
 package com.siberio;
 
+import java.util.stream.Stream;
+
 public class Maquina
 {
+	public Destino[][] caminhos;
+
+	public Maquina(Stream<String> strStream)
+	{
+
+	}
+
+	private
 }
