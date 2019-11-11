064469d6c59fc6a4780ce19dc67995f71e357aaf
ITWhiteRaccoon <eduardo.a@edu.pucrs.br>
2019-11-11 12:02:55 -0300

    Revert "maven"
    This reverts commit 8f3e6e6612c90cf6dcb5040edb72965c55fcfe8f.

---
+++ src/com/siberio/Destino.java
@@ -0,0 +1,13 @@
+package com.siberio;
+
+public class Destino
+{
+	public int cano;
+	public int altura;
+
+	public Destino(int cano, int altura)
+	{
+		this.cano = cano;
+		this.altura = altura;
+	}
+}
--- src/main/java/com/siberio/Maquina.java
+++
@@ -1,5 +0,0 @@
-package com.siberio;
-
-public class Maquina
-{
-}
