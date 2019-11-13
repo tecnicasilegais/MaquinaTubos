2c0c819f445f6f2550120179607ae425a840580c
MarceloHeredia <marcelo.heredia@acad.pucrs.br>
2019-11-13 00:10:45 -0300

    c#

---
+++ MaqTubosCs/MaquinaTubos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MaqTubosCs
+{
+    public class MaquinaTubos
+    {
+        #region Atributos
+        private long _nroTubos;
+        private long _altura;
+        private Tubo[] _tubos;
+
+        #endregion
+
+        #region Propriedades
+        #endregion
+
+        #region Construtor
+
+        public MaquinaTubos(long nroTubos, long altura)
+        {
+            this._nroTubos = nroTubos;
+            this._altura = altura; 
+            _tubos = new Tubo[nroTubos];
+            
+        }
+        #endregion
+
+        #region Metodos
+
+        public void testar(int i)
+        {
+            Console.WriteLine(
+                _tubos[i]._qtdBolas);
+        }
+
+        #endregion
+    }
+}
